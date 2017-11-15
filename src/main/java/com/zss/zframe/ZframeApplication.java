462d8184bfa7e644d630f6af492fd28ddd26fbd4
zhangmiao <615810037@qq.com>
2017-11-15 18:55:37 +0800

    优化代码

--- src/main/java/com/zss/zframe/ZframeApplication.java
+++ src/main/java/com/zss/zframe/ZframeApplication.java
@@ -3,11 +3,14 @@ package com.zss.zframe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.zss.zframe.utils.Constants;
+
 @SpringBootApplication
 public class ZframeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZframeApplication.class, args);
+		Constants.init();
 	}
 	
 }
